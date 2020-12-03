a9b547d13e7fccd9e4d6000963371600a381dd10
finixbase123 <71374279+finixbase123@users.noreply.github.com>
2020-12-03 17:19:18 +0900

    Update CleanerCommand.php

--- src/finixbase123/CleanerCommand.php
+++ src/finixbase123/CleanerCommand.php
@@ -13,7 +13,7 @@ class CleanerCommand extends Command {
   const PREFIX = TextFormat::GREEN . '* ' . TextFormat::WHITE;
 
   public function __construct() {
-    parent::__construct('클리너', '클리너에 관한 명령어입니다.', '/클리너', ['cleaner']);
+    parent::__construct('cleaner', 'cleaner', '/cleaner', []);
   }
 
   public function execute(CommandSender $sender, string $commandLabel, array $args)
@@ -21,16 +21,13 @@ class CleanerCommand extends Command {
     $name = strtolower($sender->getName());
     if(!$sender->isOp()) return false;
     if(!isset($args[0]) or !isset($args[1])) {
-      $sender->sendMessage(self::PREFIX . '/클리너 시간설정 [초 : int]');
       $sender->sendMessage(self::PREFIX . '/cleaner set [second : int]');
       return false;
     }
-    if($args[0] == '시간설정' or $args[0] =='set') {
+    if($args[0] =='set') {
       Cleaner::getInstance()->db['time'] = $args[1];
-      $sender->sendMessage(self::PREFIX . '성공적으로 아이템 클리너 시간을 ' . $args[1] . '로 설정하였습니다.');
+      $sender->sendMessage(self::PREFIX . 'Success');
       return true;
     }
 }
 }
-
-?>
